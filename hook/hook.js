80733f29f8dfc69ca72b1e63a620eef640c15196
lalilaloe <jonathantenhove@hotmail.com>
2016-03-11 14:04:13 +0100

    hook.js

--- hook/hook.js
+++ hook/hook.js
@@ -6,7 +6,7 @@ gith({
 }).on('all', function (payload) {
 	
 	console.log("push received");
-	exec('C:\Program Files\Git\NodePesten\hook\hook.bat ' + payload.branch, function (err, stdout, stderr) {
+	exec('C:\Program Files\Git\NodePesten\hook\hook.bat', function (err, stdout, stderr) {
 		if (err) return err;
 		console.log(stdout);
 		console.log("git deployed to branch " + payload.branch);
