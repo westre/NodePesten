46c914cfe825fed770082907905993392a8f1da4
lalilaloe <jonathantenhove@hotmail.com>
2016-03-11 13:51:23 +0100

    Test voor server

--- hook/hook.js
+++ hook/hook.js
@@ -1,8 +1,8 @@
-﻿var gith = require('gith').create(6000);  // run on port 6000
+﻿var gith = require('gith').create(6000);  // Run on port 6000
 var exec = require('child_process').exec;
 
 gith({
-	repo: 'westre/NodePesten'  // the github-user/repo-name
+	repo: 'westre/NodePesten'  // The github-user/repo-name
 }).on('all', function (payload) {
 	
 	console.log("push received");
